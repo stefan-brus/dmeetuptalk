c1e3d1ab9cbd0307800e6041436c22f3163957d1
Stefan Brus <stefan1brus@gmail.com>
2016-02-18 22:56:34 +0100

    Step 3: Refactor into a class with a couple of methods

--- source/app.d
+++ source/app.d
@@ -4,51 +4,78 @@ import derelict.sdl2.sdl;
 import std.exception;
 import std.string;
 
-void main ( )
+class SDLGame
 {
-	// Load Derelict's SDL bindings
-    DerelictSDL2.load();
-
-    // Load Derelict's SDL_Image bindings
-    DerelictSDL2Image.load();
-
-    // Initialize SDL
-    enum INIT_FLAGS = SDL_INIT_EVERYTHING;
-    enforce(SDL_Init(INIT_FLAGS) == 0, "Couldn't initialize SDL");
-
-    // Load our hero image
-    auto hero_img_path = toStringz("hero.jpg"); // Needs to be a C string so can't be a compile time constant
-    auto hero_surface = IMG_Load(hero_img_path);
-    enforce(hero_surface !is null, "Couldn't load hero sprite");
-
-    // Create our window where we render stuff
-    auto window_title = toStringz("An SDL game written in D!"); // Needs to be a C string so can't be a compile time constant
-    enum WINDOW_X = 100;
-    enum WINDOW_Y = 100;
+    // The window
     enum WINDOW_WIDTH = 400;
     enum WINDOW_HEIGHT = 400;
-    enum WINDOW_FLAGS = SDL_WINDOW_SHOWN;
-    auto window = SDL_CreateWindow(window_title, WINDOW_X, WINDOW_Y, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_FLAGS);
-    enforce(window !is null, "Couldn't create a window");
+    private SDL_Window* window;
 
-    // Get the window surface so we can render stuff in the window
-    auto window_surface = SDL_GetWindowSurface(window);
-    enforce(window_surface !is null, "Couldn't get the window surface");
+    // The window surface where we draw things
+    private SDL_Surface* window_surface;
 
-    // Render our hero onto the center-ish of the window surface
-    auto src_rect = null; // The source rectangle to copy from, null copies the entire surface
-    auto dst_rect = new SDL_Rect(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2); // The destination rectangle contains the x and y position of the sprite, width and height can be 0 since we are copying the whole surface
-    enforce(SDL_BlitSurface(hero_surface, src_rect, window_surface, dst_rect) == 0, "Couldn't draw the hero surface");
-    enforce(SDL_UpdateWindowSurface(window) == 0, "Couldn't update the window surface");
+    // The surface containing our hero sprite
+    private SDL_Surface* hero_surface;
 
-    // Wait 5 seconds
-    enum DELAY_TIME_MS = 5000;
-    SDL_Delay(DELAY_TIME_MS);
+    // Constructor - initializes resources
+    this ( )
+    {
+        // Load Derelict's SDL bindings
+        DerelictSDL2.load();
 
-    // Free the surfaces
-    SDL_FreeSurface(window_surface);
-    SDL_FreeSurface(hero_surface);
+        // Load Derelict's SDL_Image bindings
+        DerelictSDL2Image.load();
 
-    // Destroy the window
-    SDL_DestroyWindow(window);
+        // Initialize SDL
+        enum INIT_FLAGS = SDL_INIT_EVERYTHING;
+        enforce(SDL_Init(INIT_FLAGS) == 0, "Couldn't initialize SDL");
+
+        // Load our hero image
+        auto hero_img_path = toStringz("hero.jpg"); // Needs to be a C string so can't be a compile time constant
+        this.hero_surface = IMG_Load(hero_img_path);
+        enforce(this.hero_surface !is null, "Couldn't load hero sprite");
+
+        // Create our window where we render stuff
+        auto window_title = toStringz("An SDL game written in D!"); // Needs to be a C string so can't be a compile time constant
+        enum WINDOW_X = 100;
+        enum WINDOW_Y = 100;
+        enum WINDOW_FLAGS = SDL_WINDOW_SHOWN;
+        this.window = SDL_CreateWindow(window_title, WINDOW_X, WINDOW_Y, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_FLAGS);
+        enforce(this.window !is null, "Couldn't create a window");
+
+        // Get the window surface so we can render stuff in the window
+        this.window_surface = SDL_GetWindowSurface(this.window);
+        enforce(this.window_surface !is null, "Couldn't get the window surface");
+    }
+
+    // Destructor - frees SDL resources
+    ~this ( )
+    {
+        // Free the surfaces
+        SDL_FreeSurface(this.window_surface);
+        SDL_FreeSurface(this.hero_surface);
+
+        // Destroy the window
+        SDL_DestroyWindow(this.window);
+    }
+
+    // Run the game
+    void run ( )
+    {
+        // Render our hero onto the center-ish of the window surface
+        auto src_rect = null; // The source rectangle to copy from, null copies the entire surface
+        auto dst_rect = new SDL_Rect(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2); // The destination rectangle contains the x and y position of the sprite, width and height can be 0 since we are copying the whole surface
+        enforce(SDL_BlitSurface(this.hero_surface, src_rect, this.window_surface, dst_rect) == 0, "Couldn't draw the hero surface");
+        enforce(SDL_UpdateWindowSurface(this.window) == 0, "Couldn't update the window surface");
+
+        // Wait 5 seconds
+        enum DELAY_TIME_MS = 5000;
+        SDL_Delay(DELAY_TIME_MS);
+    }
+}
+
+void main ( )
+{
+    auto game = new SDLGame();
+    game.run();
 }
